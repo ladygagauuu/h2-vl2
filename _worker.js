c1909dc93b4a5f6934e57c7f890cb6e7c3d4e2c1
ladygagauuu <103022294+ladygagauuu@users.noreply.github.com>
2022-10-31 11:23:04 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'vlsivi.herokuapp.com'
+      url.hostname = 'vlfjdiidcs.herokuapp.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
